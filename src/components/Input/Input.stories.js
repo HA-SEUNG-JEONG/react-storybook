63ac2970b37908a16f7ddecabdb1055469b6395d
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-12 23:57:39 +0900

    Story within Story

--- src/components/Input/Input.stories.js
+++ src/components/Input/Input.stories.js
@@ -9,5 +9,3 @@ export default {
 export const Small = () => <Input size="small" placeholder="Small size" />;
 export const Medium = () => <Input size="medium" placeholder="Medium size" />;
 export const Large = () => <Input size="large" placeholder="Large size" />;
-
-Small.storyName = "Small Input";
