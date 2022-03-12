2bdf444db107e0246456ef9aa002275b096728d9
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-12 23:39:40 +0900

    Story Heirarchy

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -2,11 +2,11 @@ import React from "react";
 import Button from "./Button";
 
 export default {
-  title: "Button",
+  title: "Form/Button",
   component: Button,
 };
 
-export const Primary = () => <Button className="primary">Primary</Button>;
-export const Secondary = () => <Button className="secondary">Secondary</Button>;
-export const Success = () => <Button className="success">Success</Button>;
-export const Danger = () => <Button className="danger">Danger</Button>;
+export const Primary = () => <Button variant="primary">Primary</Button>;
+export const Secondary = () => <Button variant="secondary">Secondary</Button>;
+export const Success = () => <Button variant="success">Success</Button>;
+export const Danger = () => <Button variant="danger">Danger</Button>;
