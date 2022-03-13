cabf4a9bb4dc5a7f5cb67b02b8f394dde1fa31f6
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-13 23:34:39 +0900

    Decorators

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,5 @@
-export const parameters = {
-  options: {
-    storySort: (a, b) =>
-      a[1].kind === b[1].kind
-        ? 0
-        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
-  },
-};
+import React from "react";
+import { addDecorator } from "@storybook/react";
+import Center from "../src/components/Center/Center";
+
+addDecorator((story) => <Center>{story()}</Center>);
