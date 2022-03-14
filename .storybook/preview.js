c45390ea11a7149d2e792eefcd1a18c488605bf5
gktmd652@naver.com <88266129+HA-SEUNG-JEONG@users.noreply.github.com>
2022-03-14 13:42:30 +0900

    Console Addon

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
 import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/core";
+import { withConsole } from "@storybook/addon-console";
 
 addDecorator((story) => (
   <ThemeProvider theme={theme}>
@@ -9,3 +10,5 @@ addDecorator((story) => (
     <Box m="4">{story()}</Box>
   </ThemeProvider>
 ));
+
+addDecorator((storyFn, context) => withConsole()(storyFn)(context));
--- src/components/ChakraButton/ChakraButton.stories.js
+++ src/components/ChakraButton/ChakraButton.stories.js
@@ -1,26 +1,25 @@
 import React from "react";
 import { Button } from "@chakra-ui/core";
+import { action, actions } from "@storybook/addon-actions";
 
 export default {
   title: "Chakra/Button",
   component: Button,
-  argTypes: {
-    variantColor: { control: "text" },
-    children: { control: "text" },
-    onClick: { action: "clicked" },
-  },
 };
 
-const Template = (args) => <Button {...args} />;
+export const Success = () => (
+  <Button variantColor="green" onClick={action("Click handler")}>
+    Success
+  </Button>
+);
+export const Danger = () => (
+  <Button variantColor="red" {...actions("onClick", "onMouseOver")}>
+    Danger
+  </Button>
+);
 
-export const Success = Template.bind({});
-Success.args = {
-  variantColor: "green",
-  children: "Success",
-};
-
-export const Danger = Template.bind({});
-Danger.args = {
-  variantColor: "red",
-  children: "Danger",
-};
+export const Log = () => (
+  <Button variantColor="blue" onClick={() => console.log("Button clicked")}>
+    Log
+  </Button>
+);
